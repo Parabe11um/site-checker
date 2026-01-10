9053acf6a6c39881afe14850760db48964084684
Parabe11um <r.a.bazhenoff@gmail.com>
2026-01-10 22:08:38 +0300

    send_email fix

--- accounts/views.py
+++ accounts/views.py
@@ -9,6 +9,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import AuthenticationForm
 from django.core.mail import send_mail
 from django.contrib.auth.decorators import login_required
+from django.conf import settings
 
 from .models import User
 from .forms import RegisterForm, LoginForm
